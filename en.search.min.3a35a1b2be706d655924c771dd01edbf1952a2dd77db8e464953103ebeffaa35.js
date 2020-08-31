1b13f6b5a15fbdb5dfff64d40f9fe78cdd86e745
Andreas C Mueller <andreasmuellerml@gmail.com>
2020-08-31 18:04:47 -0400

    update website after merge from ADO

--- en.search.min.3a35a1b2be706d655924c771dd01edbf1952a2dd77db8e464953103ebeffaa35.js
+++ en.search.min.3a35a1b2be706d655924c771dd01edbf1952a2dd77db8e464953103ebeffaa35.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/MLOS/flexsearch.min.js');loadScript('/MLOS/en.search-data.min.9c64e995328022379b988e2176af930ba22ba81ab9c35f88d35844ccf0c645e5.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/MLOS/flexsearch.min.js');loadScript('/MLOS/en.search-data.min.437352f741a54bd3ee6ef9da0aac09f4c803f1751814485288d80e7cce7125af.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
